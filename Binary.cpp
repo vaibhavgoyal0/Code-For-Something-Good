332b1dc5a846f340612fedd1b07f95ff01c96bc3
Vaibhav <95163647+vaibhavgoyal0@users.noreply.github.com>
2022-10-09 19:38:43 +0530

    Add files via upload

---
+++ Binary.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int n;
+    cout << "Enter Number ";
+    cin >> n;
+    int binary = 0;
+    int reverse= 0;
+    int j=0;
+    int lastdigit = 0;
+    while(n>0) {
+        binary = n%2;
+        n = n/2;
+        cout << binary << " ";
+        // reverse the given output to obtain correct Binary Number... V@ibh@v... Signing Off...
+    }
+}
